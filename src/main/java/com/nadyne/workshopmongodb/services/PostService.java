2275b287c000effb0857739455923eef6e1c9408
Nadyne Barbieri <97990821+NadyneBarbieri@users.noreply.github.com>
2024-07-15 22:19:09 -0300

    Obtendo post por id

---
+++ src/main/java/com/nadyne/workshopmongodb/services/PostService.java
@@ -0,0 +1,30 @@
+package com.nadyne.workshopmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nadyne.workshopmongodb.domain.Post;
+import com.nadyne.workshopmongodb.domain.User;
+import com.nadyne.workshopmongodb.repository.PostRepository;
+import com.nadyne.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository post;
+	
+	
+	public Post findById(String id) { 
+		 Optional<Post> obj = post.findById(id); 
+		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+		} 
+	
+
+	
+	
+}
+
