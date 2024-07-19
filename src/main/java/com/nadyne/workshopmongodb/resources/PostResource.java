f1921b028a775630aa34c0e14da460c21774214a
Nadyne Barbieri <97990821+NadyneBarbieri@users.noreply.github.com>
2024-07-19 19:12:19 -0300

    Implementando medodo de delete de post

--- src/main/java/com/nadyne/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/nadyne/workshopmongodb/resources/PostResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nadyne.workshopmongodb.domain.Post;
+import com.nadyne.workshopmongodb.repository.PostRepository;
 import com.nadyne.workshopmongodb.resources.util.URL;
 import com.nadyne.workshopmongodb.services.PostService;
 
@@ -21,30 +23,36 @@ public class PostResource {
 
 	@Autowired
 	private PostService service;
+	@Autowired
+	private PostRepository repository;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> findByIdTitle(@RequestParam(value = "text", defaultValue="") String text) {
+	public ResponseEntity<List<Post>> findByIdTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParams(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> fullsearch(
-			@RequestParam(value = "text", defaultValue="") String text,
-			@RequestParam(value = "minDate", defaultValue="") String minDate,
-			@RequestParam(value = "maxDate", defaultValue="") String maxDate) {
+	public ResponseEntity<List<Post>> fullsearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
 		text = URL.decodeParams(text);
 		Date min = URL.convertate(minDate, new Date(0L));
 		Date max = URL.convertate(maxDate, new Date());
 		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deletePostagem(@PathVariable String id) {
+		repository.deleteById(id);
 	}
 
 }
