774a043092a9cf35d279709e7cbf4eb7d1b3f77e
Nadyne Barbieri <97990821+NadyneBarbieri@users.noreply.github.com>
2024-07-19 20:23:32 -0300

    Metodo post de postagem

--- src/main/java/com/nadyne/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/nadyne/workshopmongodb/resources/PostResource.java
@@ -4,9 +4,12 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -50,6 +53,12 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 	}
 
+	@PostMapping
+	public ResponseEntity<Post> postPostagem(@RequestBody Post postagem){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(repository.save(postagem));
+	}
+	
 	@DeleteMapping("/{id}")
 	public void deletePostagem(@PathVariable String id) {
 		repository.deleteById(id);
